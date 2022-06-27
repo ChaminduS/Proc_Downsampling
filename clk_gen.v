b8d6fb9386a449ff890637aa3a180d356f747c9e
Chamindu Sauranga <chamindu1567@gmail.com>
2022-06-27 09:24:30 +0530

    Starting clock generator

---
+++ clk_gen.v
@@ -0,0 +1,7 @@
+/***************************************************************
+EN3030 - Circuits and System Design
+180497C - 180554B - 180564F - 180574K
+Designing a custom processor for Image Downsampling
+
+Module : Clock Generator
+****************************************************************/
