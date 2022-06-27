07862f232f72d1d41cf4659e59a46a23aab8573c
Chamindu Sauranga <chamindu1567@gmail.com>
2022-06-27 09:24:57 +0530

    Added header comment

--- PC.v
+++ PC.v
@@ -1,3 +1,10 @@
+/***************************************************************
+EN3030 - Circuits and System Design
+180497C - 180554B - 180564F - 180574K
+Designing a custom processor for Image Downsampling
+
+Module : Program Counter
+****************************************************************/
 module PC(
     input clk,
     input enable,
