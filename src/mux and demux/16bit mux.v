5d2e88fc58a681a3bdc8a15a82b36296672fccb9
Prishan1997 <59108081+Prishan1997@users.noreply.github.com>
2022-06-30 09:45:37 +0530

    Add files via upload

---
+++ src/mux and demux/16bit mux.v
@@ -0,0 +1,14 @@
+module mux2to1_16bit (
+input [15:0] A,
+input [15:0] B,
+input sel ,
+output reg [15:0] OUT
+);
+always @(A or B or sel)
+    begin
+        case ( sel)
+            1'b0 : OUT = A;
+            1'b1 : OUT = B;
+        endcase
+    end
+endmodule
