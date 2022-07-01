822178ae2255f82fdacb7a4338c7ce040563c2a5
Prishan1997 <59108081+Prishan1997@users.noreply.github.com>
2022-07-01 22:30:27 +0530

    Update DRAM.v

--- src/DRAM.v
+++ src/DRAM.v
@@ -13,16 +13,24 @@ module DRAM (
     input clk,
     input write,
     input read,
+    input rd_en,
+    input wr_en,
     input [7:0] din,
+    output rd_done,
+    output wr_done,
     output reg [7:0] dout
 );
 
 reg [7:0] RAM [65535:0];
-/*****************************    
+  
 initial begin
-    $readmemb("path to the image value file",RAM);
+    if (rd_en) begin
+        $readmemb("path to the image value file",RAM);
+    end
+    rd_done=1;
+
 end
-******************************/
+
 always @(posedge clk) 
 begin
     if (write) begin
@@ -35,10 +43,13 @@ begin
     end
     
 end
-/*****************************    
+  
 initial begin
+    if (wr_en) begin
     $writememh("saving location","array that contain the final pixel values");
+    end
+    wr_done=1;
 end
-******************************/
+
     
 endmodule
