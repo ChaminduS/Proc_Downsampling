9a5498228759b00343588b7ce9d01974783a3181
Chamindu Sauranga <chamindu1567@gmail.com>
2022-07-02 00:07:05 +0530

    Edited MASTER to add connections between processor and DRAM

--- src/MASTER.v
+++ src/MASTER.v
@@ -9,6 +9,11 @@ wire wr_en;
 wire rd_done;
 wire wr_done;
 wire finish;
+wire [15:0] addr_out;
+wire [7:0] dout;
+wire read;
+wire write;
+wire [7:0] din;
 
 //State Control
 state_control state_control(
@@ -33,14 +38,24 @@ DRAM DRAM(
     .rd_en(rd_en),
     .wr_en(wr_en),
     .rd_done(rd_done),
-    .wr_done(wr_done)
+    .wr_done(wr_done),
+    .din(dout),
+    .addr(addr_out),
+    .dout(din),
+    .read(read),
+    .write(write)
 );
 
 //Processor
 processor processor(
     .clk(clk),
     .finish(finish),
-    .enable(enable)
+    .enable(enable),
+    .d_in(din),
+    .addr_out(addr_out),
+    .dout(dout),
+    .read(read),
+    .write(write)
 );
 
 endmodule
