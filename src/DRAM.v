6f38d920480b045af86bc3c0bfaec9d2a09a6847
Chamindu Sauranga <chamindu1567@gmail.com>
2022-07-01 23:20:44 +0530

    DRAM rebuilt as a state machine. Compilation successful. Not simulated

--- src/DRAM.v
+++ src/DRAM.v
@@ -16,40 +16,61 @@ module DRAM (
     input rd_en,
     input wr_en,
     input [7:0] din,
-    output rd_done,
-    output wr_done,
+    output reg rd_done,
+    output reg wr_done,
     output reg [7:0] dout
 );
 
 reg [7:0] RAM [65535:0];
   
-initial begin
-    if (rd_en) begin
-        $readmemb("path to the image value file",RAM);
-    end
-    rd_done=1;
 
-end
+parameter IDLE = 2'b00;
+parameter DRAM_rd = 2'b01;
+parameter proc_run = 2'b10;
+parameter DRAM_wr = 2'b11;
 
-always @(posedge clk) 
-begin
-    if (write) begin
-        RAM[addr] <= din;
-    
-    end
-    if (read) begin
-        dout <= RAM[addr];
+reg [2:0] state = IDLE;
+
+
+always @(posedge clk) begin
+    case (state)
+        IDLE :
+            begin
+                if (rd_en) begin
+                    state <= DRAM_rd;
+                end
+            end
         
-    end
-    
+        DRAM_rd :
+            begin
+                $readmemb("path to the image value file",RAM);
+                rd_done <= 1'b1;
+                state <= proc_run;
+            end
+
+        proc_run :
+            begin
+                if (~wr_en)
+                    if (write) begin
+                        RAM[addr] <= din;
+                    end
+
+                    if (read) begin
+                        dout <= RAM[addr];
+                    end
+                else state <=DRAM_wr;
+            end
+
+        DRAM_wr :
+            begin
+                $writememh("saving location","array that contain the final pixel values");
+                wr_done=1;
+                state <= IDLE;
+            end
+
+    endcase
 end
-  
-initial begin
-    if (wr_en) begin
-    $writememh("saving location","array that contain the final pixel values");
-    end
-    wr_done=1;
-end
+
 
     
 endmodule
